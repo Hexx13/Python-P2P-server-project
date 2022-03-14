e8bf0d27b91859a47bd4f88e50f3f1b742c372cb
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2022-03-14 21:47:35 +0000

    project finished

--- client-server/c.py
+++ client-server/c.py
@@ -1,42 +1,148 @@
-
 # Echo client program
 import socket
 
-HOST = '127.0.0.1'    # The remote host
-PORT = 50007          # The same port as used by the server
+HOST = '127.0.0.1'  # The remote host
+PORT = 50007  # The same port as used by the server
 
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-print("connecting to server.....")
+exit = 0
 
-try:
+def playSong():
+    print("Please type which file you would like to play")
+    import webbrowser
+    webbrowser.open(input())
+
+
+def addSong(filename):
+    print("Loading file send....")
+
+    # read chunk
+    fileChunk = open(filename, 'rb')
+    # store chunk
+    theStuff = fileChunk.read()
+    # send to server
+    s.sendall(theStuff)
+    # close chunk
+    fileChunk.close()
+
+#client side recieving song from server
+def getSong():
+    # stuff = str(data).split("'")
+    print("Please choose a name for the downloading file")
+
+    filename = input() + '.mp3'
+    print(f"Downloading: {filename} ..............")
+    f = open(filename, 'wb')
+    data = s.recv(1000)
+
+    while data:
+        print(data)
+        f.write(data)
+        data = s.recv(1000)
+
+    f.close()
+
+
+cmdArr = ["servertime", "hello", "search", "addsong", "removesong", "getsong",
+          "hash", "slice", "playsong"]
+
+
+def buildMenu():
+    print("Input a number corresponding to the commands below")
+    # Array of commands
+
+    numbr = 0;
+    # Prints the array of commands
+    for i in cmdArr:
+        numbr += 1
+        print(f"{numbr} - {i}")
+
+
+def menu():
+    buildMenu()
+
+    inpt = int(input())
+
+    if 4 > inpt > 0:
+        s.sendall(("<" + cmdArr[inpt - 1] + ":").encode())
+        print("Response:" + str(s.recv(1000)))
+    elif inpt == 4:
+        print("Please select name for file")
+        name = input()
+        s.sendall(("<" + cmdArr[inpt - 1] + "-" + name + ":").encode())
+        print("Please type the filepath of the song")
+        addSong(input())
+    elif inpt == 5:
+        print("Please select a file to remove")
+        name = input()
+        s.sendall(("<" + cmdArr[inpt - 1] + "-" + name + ":").encode())
+        print("Please type the filepath of the song")
+        print("Response:" + str(s.recv(1000)))
+    elif inpt == 6:
+        print("Please select a file to download")
+        name = input()
+        s.sendall(("<" + cmdArr[inpt - 1] + "-" + name + ":").encode())
+        getSong()
+    elif inpt == 7:
+        print("Please select a file to hash")
+        name = input()
+        s.sendall(("<" + cmdArr[inpt - 1] + "-" + name + ":").encode())
+        print("Response:" + str(s.recv(1000)))
+    elif inpt == 8:
+        print("Please select a song to slice")
+        name = input()
+        s.sendall(("<" + cmdArr[inpt - 1] + "-" + name + ":").encode())
+        print("Response:" + str(s.recv(1000)))
+    elif inpt == 9:
+        playSong()
+    else:
+        print("Not an option, try again")
+        menu()
+
+
+def cli():
     print("Type help if you want the command list")
     print("type input:")
 
     text = input()
 
-
-    # when we send data to the server, we are using a colon
-    # at the end of a sentence to mark the end of the current sentence
-    # later when the input comes back, we will then be breaking the input
-    # into individual parts using the colon : to separate the lines
-    s.sendall(("<" + text + ">" + ":").encode())
-
+    s.sendall(("<" + text + ":").encode())
     if "addsong" in text:
-        "Loading file send...."
+        print("Please type filepath to song you want to add")
+        addSong(input())
+    elif "playsong" in text:
+        playSong()
+    elif "getsong" in text:
+        getSong()
+    else:
+        print("Response:" + str(s.recv(1000)))
 
-        fileChunk = open('c0part.mp3', 'rb')
-        theStuff = fileChunk.read()
-        s.sendall(theStuff)
-        fileChunk.close()
+
+def mode():
+    print("Input 1 if you wish to use the menu system")
+    print("Input 2 if you wish to use the command-line system")
+    print("Input 3 if you want to exit the system")
+    mode = input()
+    if "1" in mode:
+        menu()
+    elif "2" in mode:
+        cli()
+    elif "3" in mode:
+        global exit
+        exit = 1
+    else:
+        print("Not an option, try again")
+        mode()
 
 
-    data = s.recv(80000)
+try:
 
-
-    print("Response:" + str(data))
+    while exit == 0:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((HOST, PORT))
+        print("connecting to server.....")
+        mode()
 finally:
     print("closing connection.....")
     s.close()
--- client-server/s.py
+++ client-server/s.py
@@ -1,31 +1,90 @@
+import hashlib
+import os
 import socket
 import threading
-
+from pydub import AudioSegment
+from pydub.utils import make_chunks
 from time import gmtime, strftime
-import time
 
 HOST = '127.0.0.1'
 PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 
-# This is the buffer string
-# when input comes in from a client it is added
-# into the buffer string to be relayed later
-# to different clients that have connected
-# Each message in the buffer is separated by a colon :
-buffer = ""
-cmdArr = ["servertime", "hello", "help"]
+cmdArr = ["servertime", "hello", "help", "addsong-(songName)", "removesong-(songname)", "getsong-(songname)",
+          "hash-(songname)", "search", "slice-(songname)", "playsong"]
 songs = list()
 
-#todo make this be able handle other files apart from c0.mp3
+
+def hash(data, con):
+    hasher = hashlib.sha256()
+    name = process(data);
+
+    # open file
+    file = open(name, 'rb')
+    content = file.read()
+
+    # hash
+    hasher.update(content)
+    result = hasher.digest()
+
+    con.send(str(result).encode())
+
+# splits commands and returns the command extension
+def process(data):
+    arr = data.split('-')
+
+    process1 = arr[1].translate({ord(':'): None})
+    return process1.translate({ord("'"): None})
+
+# lists songs on server
+def search(con):
+    files = os.listdir(path=os.getcwd())
+
+    mp3s = list()
+
+    for oneFile in files:
+        if ".mp3" in oneFile:
+            print(oneFile)
+            mp3s.append(oneFile)
+
+    con.send(str(mp3s).encode())
+
+# custom command, deletes a song
+def removeSong(data, con):
+    print("song being removed......")
+
+    if ".mp3" in str(data):
+        os.remove(process(data))
+        con.send("Song deleted".encode())
+
+    else:
+        con.send("Invalid file type".encode())
+
+# server send song to client
+def getSong(data, con):
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    print("get file......")
+
+    fileName = process(data)
+
+    print("filename: " + str(fileName))
+    print(f"sending file to {hostname}  {ip_address} ..................")
+
+    f = open(fileName, 'rb')
+    content = f.read()
+    con.sendall(content)
+    f.close()
+    print("file recieved by client")
+
+# client send song to server
 def addSong(data, con):
     print("sond being added......")
-    arr = data.split('-')
-    for x in arr:
-        print(x)
-
-    f = open('c0.mp3', 'wb')
+    name = process(data)
+    filename = name + '.mp3'
+    print(filename)
+    f = open(filename, 'wb')
 
     filedata = con.recv(1000)
     while filedata:
@@ -34,74 +93,102 @@ def addSong(data, con):
         filedata = con.recv(1000)
 
     f.close()
-    songs.append(arr[1])
+    songs.append(name)
 
 # custom say hello command
 def sayHello(con):
     print("----> The hello function was called")
     con.send("Hello from the server".encode())
 
-
 # custom command list command
-# S ends the list of commands to the client
+# Sends the list of commands to the client
 def cmdList(con):
     messg = "Commands list: "
     for x in cmdArr:
         messg += x + ";    "
     con.send(messg.encode())
 
-
 def getServTime(con):
     print("command in data..")
-    formatted = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+    con.send(str(strftime("%a, %d %b %Y %H:%M:%S", gmtime())).encode())
 
-    con.send(str(formatted).encode())
+def createLog():
+    logName = "Session " + str(strftime("%a %d %b %Y %H.%M.%S", gmtime())) + " HostName " + socket.gethostname() + ".txt"
+    log = open(logName, 'w')
+    log.close()
+    return logName
 
+def slice(data, con):
+    print("get file......")
+    fileName = process(data)
+    print("slicing file......")
+    myAudio = AudioSegment.from_file(fileName, "mp3")
+    chunk_length_ms = 1000 * 30  # pydub calculates in millisec
+    chunks = make_chunks(myAudio, chunk_length_ms)  # Make chunks of 30 seconds
 
-# sample parser function. The job of this function is to take some input
-# data and search to see if a command is present in the text. If it finds a 
-# command it will then need to extract the command.
+    # Export all of the individual chunks as mp3 files
+
+    for i, chunk in enumerate(chunks):
+        chunk_name = "chunk{0}.mp3".format(i)
+        chunk.export(chunk_name, format="mp3")
+
+    con.sendall("Slicing finished".encode())
+
 def parseInput(data, con):
     print("parsing...")
     print(str(data))
 
     # Checking for commands
-    if "<getservertime>" in data:
+    if "<getservertime" in data:
         getServTime(con)
-    elif "<help>" in data:
+    elif "<help" in data:
         cmdList(con)
-    elif "<hello>" in data:
+    elif "<hello" in data:
         sayHello(con)
-
-
-# we a new thread is started from an incoming connection
-# the manageConnection funnction is used to take the input
-# and print it out on the server
-# the data that came in from a client is added to the buffer.
+    elif "<addsong" in data:
+        addSong(data, con)
+    elif "<removesong" in data:
+        removeSong(data, con)
+    elif "<search" in data:
+        search(con)
+    elif "<getsong" in data:
+        getSong(data, con)
+    elif "<hash" in data:
+        hash(data, con)
+    elif "<slice" in data:
+        slice(data, con)
 
 def manageConnection(conn, addr):
-    global buffer
+
     print('Connected by', addr)
+
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+
+    #creates log file
+    logName = createLog()
+    #open log file
+    log = open(logName, 'w')
+    #log ip and hostname
+    log.write("Host connected: " + hostname + "Ip address: " + ip_address+"\r\n")
 
     data = conn.recv(1024)
 
-    parseInput(str(data), conn)  # Calling the parser, passing the connection
+    parseInput(str(data).lower(), conn)  # Calling the parser, passing the connection
+    inpt = "rec:" + str(data)
+    #log input
+    log.write(inpt+"\r\n")
+    print(inpt)
 
-    print("rec:" + str(data))
-    buffer += str(data)
-
-    # conn.send(str(buffer))
-
+    log.close()
     conn.close()
 
 
 while 1:
     s.listen(1)
     conn, addr = s.accept()
-    # after we have listened and accepted a connection coming in,
-    # we will then create a thread for that incoming connection.
-    # this will prevent us from blocking the listening process
-    # which would prevent further incoming connections
+    # Threading was implemented on the server-side to not interrupt the listening function for further connections.
+    # This way the server can operate normally while clients are able to connect.
     t = threading.Thread(target=manageConnection, args=(conn, addr))
 
     t.start()
